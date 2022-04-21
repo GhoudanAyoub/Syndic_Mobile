7cf5ca989ede51bd1b57e29ba90e22305b235b90
ayoub <ayoubghoudanos@gmail.com>
2022-04-21 18:02:05 +0000

    Networking Hilt

---
+++ app/src/main/java/com/SyndicG5/MainApp.java
@@ -0,0 +1,9 @@
+package com.SyndicG5;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MainApp extends Application {
+}
---
+++ networking/src/main/java/com/syndicg5/networking/api/ClientAPI.java
@@ -0,0 +1,32 @@
+package com.syndicg5.networking.api;
+
+
+
+import static com.syndicg5.networking.utils.Commun.IP;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.internal.managers.ApplicationComponentManager;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+@InstallIn(ApplicationComponentManager.class)
+public class ClientAPI {
+
+    @Provides
+    @Singleton
+    public static APISettings getInstance() {
+        String base_url = "http://"+IP+"/";
+        return new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(APISettings.class);
+    }
+}
