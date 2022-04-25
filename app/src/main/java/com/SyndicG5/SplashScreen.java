826de8fb261f54982503bd48a509774f6adfbb5c
ayoub <ayoubghoudanos@gmail.com>
2022-04-25 15:33:28 +0000

    Verify Cache First

--- app/src/main/java/com/SyndicG5/SplashScreen.java
+++ app/src/main/java/com/SyndicG5/SplashScreen.java
@@ -1,13 +1,16 @@
 package com.SyndicG5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.SyndicG5.ui.home.HomeContainer;
 import com.SyndicG5.ui.login.login;
+import com.SyndicG5.ui.login.loginViewModel;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,14 +19,41 @@ import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 
+@SuppressLint("CheckResult")
+@AndroidEntryPoint
 public class SplashScreen extends AppCompatActivity {
 
-    @SuppressLint("CheckResult")
+    private loginViewModel mViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mViewModel = new ViewModelProvider(this).get(loginViewModel.class);
+        mViewModel.getLoginInfo();
+        mViewModel.getLoginLiveData().observe(this, it -> {
+            if (it != null) {
+                if (it.getEmail()!=null)
+                    goHome();
+                else {
+                    goLogin();
+                }
+            } else {
+                goLogin();
+            }
+        });
+    }
+
+
+    private void goHome() {
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    startActivity(new Intent(getApplication(), HomeContainer.class));
+                });
+    }
+
+    private void goLogin() {
         Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
                 .subscribe(() -> {
                     startActivity(new Intent(getApplication(), login.class));
