f0d74930db0c56683d59cc2c22107bcabed78b87
ayoub <ayoubghoudanos@gmail.com>
2022-04-25 14:57:40 +0000

    View Models

---
+++ app/src/main/java/com/SyndicG5/ui/home/fragments/home/HomefragementViewModel.java
@@ -0,0 +1,7 @@
+package com.SyndicG5.ui.home.fragments.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomefragementViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
