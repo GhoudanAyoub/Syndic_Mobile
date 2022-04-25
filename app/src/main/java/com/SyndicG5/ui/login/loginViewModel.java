818d33466df17892233eb3eef2f2e6dd45e31e97
ayoub <ayoubghoudanos@gmail.com>
2022-04-25 15:32:41 +0000

    User Cache

--- app/src/main/java/com/SyndicG5/ui/login/loginViewModel.java
+++ app/src/main/java/com/SyndicG5/ui/login/loginViewModel.java
@@ -1,17 +1,41 @@
 package com.SyndicG5.ui.login;
 
+import androidx.hilt.lifecycle.ViewModelInject;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.syndicg5.networking.models.User;
+import com.syndicg5.networking.repository.apiRepository;
+import com.syndicg5.networking.repository.roomRepository;
+
 public class loginViewModel extends ViewModel {
 
+    private roomRepository roomRepo;
+    private apiRepository apiRepository;
+
     private MutableLiveData<Boolean> booleanMutableLiveData = new MutableLiveData<>();
+    private LiveData<User> loginLiveData = new MutableLiveData<>();
+
+    public LiveData<User> getLoginLiveData() {
+        return loginLiveData;
+    }
 
     public MutableLiveData<Boolean> getBooleanMutableLiveData() {
         return booleanMutableLiveData;
     }
 
+    @ViewModelInject
+    public loginViewModel(roomRepository roomRepository, apiRepository apiRepository) {
+        this.roomRepo = roomRepository;
+        this.apiRepository = apiRepository;
+    }
+
     public void Login(String email, String pass) {
         booleanMutableLiveData.setValue(true);
+    }
+
+    public void getLoginInfo() {
+        loginLiveData = roomRepo.getLoginInfo();
     }
 }
--- networking/src/main/java/com/syndicg5/networking/di/dao.java
+++ networking/src/main/java/com/syndicg5/networking/di/dao.java
@@ -1,8 +1,23 @@
 package com.syndicg5.networking.di;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.syndicg5.networking.models.User;
 
 @Dao
 public interface dao {
+
+    @Query("Select * from user")
+    LiveData<User> getLoginInfo();
+
+    @Insert
+    void saveLogin(User f);
+
+    @Update
+    void UpdateLogin(User f);
 
 }
