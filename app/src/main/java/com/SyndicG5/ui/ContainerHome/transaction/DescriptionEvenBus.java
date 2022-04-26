33932e075811b9f4a43d441a447c75dc32ef575e
ayoub <ayoubghoudanos@gmail.com>
2022-04-26 01:07:16 +0000

    Transaction Activity (WIP)

---
+++ app/src/main/java/com/SyndicG5/ui/ContainerHome/transaction/DescriptionEvenBus.java
@@ -0,0 +1,17 @@
+package com.SyndicG5.ui.ContainerHome.transaction;
+
+public class DescriptionEvenBus {
+    private String text;
+
+    public DescriptionEvenBus(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
