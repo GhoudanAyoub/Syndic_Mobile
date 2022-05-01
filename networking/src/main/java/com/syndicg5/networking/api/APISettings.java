0359281d77dd3a3d9ca80fe14d24b85cd4696815
ayoub <ayoubghoudanos@gmail.com>
2022-05-01 14:55:13 +0000

    Adapt Model

--- networking/src/main/java/com/syndicg5/networking/api/APISettings.java
+++ networking/src/main/java/com/syndicg5/networking/api/APISettings.java
@@ -1,5 +1,30 @@
 package com.syndicg5.networking.api;
 
+import com.syndicg5.networking.models.Syndic;
+import com.syndicg5.networking.models.User;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface APISettings {
 
+
+    @POST("login")
+    Single<Response<ResponseBody>> Login(@Query("username") String username, @Query("password") String password);
+
+    @GET("syndics")
+    Single<List<Syndic>> getAllSyndic();
+
+    @GET("machines/byMarque")
+    Single<List<byMarque>> getByMarque();
+
+    @GET("machines/byYearApi")
+    Single<List<byMarque>> getByYearApi();
 }
--- networking/src/main/java/com/syndicg5/networking/models/Syndic.java
+++ networking/src/main/java/com/syndicg5/networking/models/Syndic.java
@@ -11,20 +11,26 @@ public class Syndic implements Serializable {
 
     @PrimaryKey(autoGenerate = true)
     private String id;
-    private Double salaire;
-    private Personne personne;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+    private String adresse;
+    private String ville;
+    private String photo;
+    private String telephone;
     private List<Immeuble> immeubles;
 
-    public Syndic(Double salaire, Personne personne, List<Immeuble> immeubles) {
-        this.salaire = salaire;
-        this.personne = personne;
-        this.immeubles = immeubles;
-    }
-
-    public Syndic(String id, Double salaire, Personne personne, List<Immeuble> immeubles) {
+    public Syndic(String id, String nom, String prenom, String email, String mdp, String adresse, String ville, String photo, String telephone, List<Immeuble> immeubles) {
         this.id = id;
-        this.salaire = salaire;
-        this.personne = personne;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.photo = photo;
+        this.telephone = telephone;
         this.immeubles = immeubles;
     }
 
@@ -36,20 +42,68 @@ public class Syndic implements Serializable {
         this.id = id;
     }
 
-    public Double getSalaire() {
-        return salaire;
+    public String getNom() {
+        return nom;
     }
 
-    public void setSalaire(Double salaire) {
-        this.salaire = salaire;
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 
-    public Personne getPersonne() {
-        return personne;
+    public String getPrenom() {
+        return prenom;
     }
 
-    public void setPersonne(Personne personne) {
-        this.personne = personne;
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public List<Immeuble> getImmeubles() {
