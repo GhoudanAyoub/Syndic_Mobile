beb237842ca9f294982d18461fe8d4c8eec60434
ayoub <ayoubghoudanos@gmail.com>
2022-05-02 22:12:40 +0000

    ViewModel

--- app/src/main/java/com/SyndicG5/ui/ContainerHome/fragments/home/HomefragementViewModel.java
+++ app/src/main/java/com/SyndicG5/ui/ContainerHome/fragments/home/HomefragementViewModel.java
@@ -1,7 +1,84 @@
 package com.SyndicG5.ui.ContainerHome.fragments.home;
 
+import android.annotation.SuppressLint;
+
+import androidx.hilt.lifecycle.ViewModelInject;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.syndicg5.networking.models.Appartement;
+import com.syndicg5.networking.models.Immeuble;
+import com.syndicg5.networking.repository.apiRepository;
+import com.syndicg5.networking.repository.roomRepository;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+@SuppressLint("CheckResult")
 public class HomefragementViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private apiRepository repository;
+    private roomRepository roomRepo;
+
+    private MutableLiveData<List<Appartement>> listAppartementMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<Immeuble>> listImmeubleMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<Appartement> AppartementMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<Immeuble> ImmeubleMutableLiveData = new MutableLiveData<>();
+
+    public MutableLiveData<List<Appartement>> getListAppartementMutableLiveData() {
+        return listAppartementMutableLiveData;
+    }
+
+    public MutableLiveData<List<Immeuble>> getListImmeubleMutableLiveData() {
+        return listImmeubleMutableLiveData;
+    }
+
+    public MutableLiveData<Appartement> getAppartementMutableLiveData() {
+        return AppartementMutableLiveData;
+    }
+
+    public MutableLiveData<Immeuble> getImmeubleMutableLiveData() {
+        return ImmeubleMutableLiveData;
+    }
+
+    @ViewModelInject
+    public HomefragementViewModel(apiRepository repository, roomRepository roomRepo) {
+        this.repository = repository;
+        this.roomRepo = roomRepo;
+    }
+
+    public void getAllAppartements() {
+        repository.getAllAppartements()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> listAppartementMutableLiveData.setValue(response)
+                        , Throwable::printStackTrace);
+    }
+
+    public void getOneAppartement(int id) {
+        repository.getOneAppartement(id)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> AppartementMutableLiveData.setValue(response)
+                        , Throwable::printStackTrace);
+    }
+
+    public void getAllImmeuble() {
+        repository.getAllImmeuble()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> listImmeubleMutableLiveData.setValue(response)
+                        , Throwable::printStackTrace);
+    }
+
+    public void getOneImmeuble(int id) {
+        repository.getOneImmeuble(id)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(response -> ImmeubleMutableLiveData.setValue(response)
+                        , Throwable::printStackTrace);
+    }
+
 }
