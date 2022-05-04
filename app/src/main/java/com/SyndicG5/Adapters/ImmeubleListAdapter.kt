318543dc50dc25a33352d9685e16750658d4a5a1
ayoub <ayoubghoudanos@gmail.com>
2022-05-04 01:00:37 +0000

    Immeuble Adapter

---
+++ app/src/main/java/com/SyndicG5/Adapters/ImmeubleListAdapter.kt
@@ -0,0 +1,86 @@
+package com.SyndicG5.Adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.SyndicG5.databinding.SelectedRadioButtonItemBinding
+import com.SyndicG5.ui.util.KarnyRadioButton
+import com.syndicg5.networking.models.Immeuble
+
+class ImmeubleListAdapter:
+    RecyclerView.Adapter<ImmeubleListAdapter.ImmeubleItemViewHolder>() {
+
+    private var ImmeublesList: List<Immeuble>? = null
+    private var checkedItems = arrayListOf<SelectedRadioButtonItemBinding>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImmeubleItemViewHolder {
+
+        val binding = SelectedRadioButtonItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ImmeubleItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImmeubleItemViewHolder, position: Int) {
+        val Immeuble = ImmeublesList?.get(position)
+        Immeuble?.let {
+            holder.bind(
+                KarnyRadioButton.KarnyRadioButtonData(
+                    title = it.nom
+                ),
+            )
+
+            holder.itemView.setOnClickListener {
+                setImmeubleClickListener?.let {
+                    it(Immeuble)
+                    checkedItems[position].root.isChecked(true)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ImmeublesList?.size ?: 0
+    }
+
+    fun setImmeublesList(Immeubles: List<Immeuble>) {
+        this.ImmeublesList = Immeubles
+        notifyDataSetChanged()
+    }
+
+    fun setAllItemsChecked(isChecked: Boolean) {
+        checkedItems.forEachIndexed { index, item ->
+            item.root.isChecked(isChecked)
+            notifyItemChanged(index)
+        }
+    }
+
+    private var setImmeubleClickListener: ((Immeuble: Immeuble) -> Unit)? = null
+
+    fun onImmeubleClicked(listener: (Immeuble) -> Unit) {
+        setImmeubleClickListener = listener
+    }
+
+    inner class ImmeubleItemViewHolder(private val ImmeubleItemBinding: SelectedRadioButtonItemBinding) :
+        RecyclerView.ViewHolder(ImmeubleItemBinding.root) {
+
+
+        fun bind(ImmeubleItemData: KarnyRadioButton.KarnyRadioButtonData) {
+            checkedItems.add(ImmeubleItemBinding)
+            ImmeubleItemBinding.root.bindView(ImmeubleItemData) { isChecked ->
+                if (isChecked) {
+                    for (ImmeubleItem in checkedItems) {
+                        if (ImmeubleItem != ImmeubleItemBinding) {
+                            ImmeubleItem.root.isChecked(false)
+                        } else {
+                            ImmeubleItem.root.isChecked(true)
+                           // callback.invoke()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
