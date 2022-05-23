a90021cf1e0358bd14693fd0c399d4f75f6abb63
ayoub <ayoubghoudanos@gmail.com>
2022-05-23 22:53:11 +0100

    appart per resident

--- app/src/main/java/com/SyndicG5/Adapters/ResidentAdapter.java
+++ app/src/main/java/com/SyndicG5/Adapters/ResidentAdapter.java
@@ -4,6 +4,7 @@ import static com.syndicg5.networking.utils.Commun.getClientNameInitials;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
@@ -15,22 +16,33 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.SyndicG5.R;
 import com.bumptech.glide.Glide;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.syndicg5.networking.models.Appartement;
 import com.syndicg5.networking.models.Resident;
+import com.syndicg5.networking.models.Revenu;
+import com.syndicg5.networking.repository.apiRepository;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.reactivex.schedulers.Schedulers;
 
 public class ResidentAdapter extends RecyclerView.Adapter<ResidentAdapter.ResidentAdapterHolder> {
 
     private ArrayList<Resident> ResidentList = new ArrayList<>();
     private Context context;
+    private apiRepository repository;
 
-    public ResidentAdapter(Context context) {
+    public ResidentAdapter(Context context, apiRepository repository) {
         this.context = context;
+        this.repository=repository;
     }
 
     @NonNull
@@ -40,6 +52,7 @@ public class ResidentAdapter extends RecyclerView.Adapter<ResidentAdapter.Reside
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onBindViewHolder(@NonNull ResidentAdapterHolder holder, int position) {
         Resident resident = ResidentList.get(position);
@@ -50,17 +63,34 @@ public class ResidentAdapter extends RecyclerView.Adapter<ResidentAdapter.Reside
         holder.transactionAmount.setTextColor(Color.GRAY);
         holder.client_name_shortcut.setText(getClientNameInitials(resident.getNom()));
         holder.itemView.setOnClickListener(view -> {
-        });
+            if (holder.hiddenView.getVisibility() == View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(holder.cardView,
+                        new AutoTransition());
+                holder.hiddenView.setVisibility(View.GONE);
+            } else {
 
-        if (resident.getPhoto() != null) {
-            Glide.with(context)
-                    .load(resident.getPhoto())
-                    .into(holder.residentImage);
-            holder.client_name_shortcut.setVisibility(View.GONE);
-        } else
-            holder.client_name_shortcut.setVisibility(View.VISIBLE);
+                TransitionManager.beginDelayedTransition(holder.cardView,
+                        new AutoTransition());
+                holder.hiddenView.setVisibility(View.VISIBLE);
+            }
+        });
+        List<Appartement> appartements = getAppartementByResident(resident.getId());
+        if (appartements != null) {
+            holder.residentImage.setVisibility(View.GONE);
+            if(appartements.get(0).getDebut()!=null)holder.dateEntrer.setText("Entrer:\n"+new SimpleDateFormat("yyyy-MM-dd").format(appartements.get(0).getDebut()));
+            if(appartements.get(0).getFin()!=null)holder.dateSortie.setText("Sortie:\n"+new SimpleDateFormat("yyyy-MM-dd").format(appartements.get(0).getFin()));
+            holder.AppartmentNum.setText("Appartement : "+appartements.get(0).getNumero()+"");
+        } else {
+            holder.residentName.setText("Appartement Vide");
+            holder.residentImage.setVisibility(View.GONE);
+        }
     }
 
+    public List<Appartement> getAppartementByResident(int id){
+        return new ArrayList<>(repository.getAppartementByResident(id)
+                .subscribeOn(Schedulers.io()).blockingGet());
+
+    }
     @Override
     public int getItemCount() {
         return ResidentList.size();
@@ -72,7 +102,7 @@ public class ResidentAdapter extends RecyclerView.Adapter<ResidentAdapter.Reside
     }
 
     public class ResidentAdapterHolder extends RecyclerView.ViewHolder {
-        public TextView transactionContact, transactionAmount, transactionDate, client_name_shortcut, residentName;
+        public TextView transactionContact, transactionAmount, transactionDate, client_name_shortcut, residentName,AppartmentNum,dateEntrer,dateSortie;
         public ShapeableImageView client_image;
         public LinearLayout hiddenView;
         public RelativeLayout cardView;
@@ -88,6 +118,9 @@ public class ResidentAdapter extends RecyclerView.Adapter<ResidentAdapter.Reside
             hiddenView = itemView.findViewById(R.id.hidden_view);
             cardView = itemView.findViewById(R.id.cardView);
             residentName = itemView.findViewById(R.id.residentName);
+            AppartmentNum = itemView.findViewById(R.id.AppartmentNum);
+            dateEntrer = itemView.findViewById(R.id.dateEntrer);
+            dateSortie = itemView.findViewById(R.id.dateSortie);
             residentImage = itemView.findViewById(R.id.residentImage);
         }
     }
